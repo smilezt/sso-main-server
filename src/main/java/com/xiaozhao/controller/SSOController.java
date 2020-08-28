845ac33fa0b7136264cedd77ed0d2c1067761010
smilezt <zhaoting1997@126.com>
2020-08-28 15:34:06 +0800

    commit

--- src/main/java/com/xiaozhao/controller/SSOController.java
+++ src/main/java/com/xiaozhao/controller/SSOController.java
@@ -2,7 +2,7 @@ package com.xiaozhao.controller;
 
 import com.xiaozhao.pojo.User;
 import com.xiaozhao.service.UserService;
-import com.xiaozhao.util.JwtUtil;
+import com.xiaozhao.util.CryptoUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author 赵婷
@@ -26,31 +24,25 @@ public class SSOController {
     UserService userService;
 
     //存储token的列表
-    List<String> list = new ArrayList<String>();
+    Map<String,String> list = new HashMap<>();
 
     /**
      * 查看是否已经在主系统上登录
+     *
      * @param url
      * @param request
      * @param model
      * @return
      */
-    @RequestMapping(value = "/preLogin",method = RequestMethod.GET)
+    @RequestMapping(value = "/preLogin", method = RequestMethod.GET)
     public String preLogin(String url, HttpServletRequest request, Model model) {
         System.out.println(url);
-        HttpSession session = request.getSession(false);
-        //发现认证中心没有全局的session会话，或者token已经被移除（登出）
-        if (session == null || session.getAttribute("token") == null) {
-            model.addAttribute("url",url);
-            return "login";
-        } else {//已经登录过
-            String token= (String) session.getAttribute("token");
-            return "redirect:http://"+url+"?token=" + token;
-        }
+        return "login";
     }
 
     /**
      * 登录系统
+     *
      * @param username
      * @param password
      * @param url
@@ -62,41 +54,35 @@ public class SSOController {
     @RequestMapping("/login")
     public String login(String username, String password, String url, HttpServletRequest request, Model model) throws Exception {
         //验证账户密码的正确性
-        if(userService.login(username,password)){
-            String token= null;
+        if (userService.login(username, password)) {
+            String token = null;
             //获得用户信息
             User user = userService.getUserInfo(username);
-            try {
-                //创建token
-                token = JwtUtil.createJwt(user.getUserName(),user.getUserId());
-            } catch (UnsupportedEncodingException e) {
-                throw new Exception("token creat fail");
-            }
+            //创建token
+            token = CryptoUtil.encode(user.getUserName());
             System.out.println(token);
-            //全局seesion中设置token
-            request.getSession().setAttribute("token", token);
-            list.add(token);
-            //http://www.user.com:8081/user/wel?token=sdfafddfdfa
-            //给子系统颁发token
-            return "redirect:http://"+url+"?token=" + token;
+            list.put(username,token);
+            model.addAttribute("userName", user.getUserName());
+            return "wel";
         } else {
             //账户密码不匹配，重新输入进行登录
-            model.addAttribute("url",url);
+            model.addAttribute("url", url);
             return "login";
         }
     }
 
     /**
      * 检验token的正确性
+     *
      * @param token
      * @return
      */
     @RequestMapping("/checkToken")
     @ResponseBody
     public String checkToken(String token) {
-        System.out.println(JwtUtil.verifyJwt(token));
         //不是伪造的，存在于token列表中且是合法有效的
-        if (list.contains(token)&&JwtUtil.verifyJwt(token)) {
+        String userName = CryptoUtil.decode(token);
+        if (list.containsKey(userName)) {
             return "correct";
         }
         return "incorrect";
@@ -104,19 +90,25 @@ public class SSOController {
 
     /**
      * 登出
+     *
      * @param request
      * @param url
      * @return
      */
     @RequestMapping("/logout")
-    public String logout(HttpServletRequest request, String url){
-        HttpSession session = request.getSession(false);
+    public String logout(HttpServletRequest request, String url) {
         //清空token列表
         list.clear();
-        //清除session中的token
-        session.removeAttribute("token");
         //重定向
-        return "redirect:http://"+url;
+        return "redirect:http://" + url;
+    }
+
+    @RequestMapping("/toUser")
+    public String toUser(HttpServletRequest request, String userName) {
+        //清空token列表
+        String token = list.get(userName);
+        //重定向
+        return "redirect:http://www.user.com:8081/user/wel?token="+token;
     }
 }
 
--- src/main/java/com/xiaozhao/interceptor/SessionInterceptor.java
+++ src/main/java/com/xiaozhao/interceptor/SessionInterceptor.java
@@ -1,11 +1,9 @@
 package com.xiaozhao.interceptor;
 
-import com.xiaozhao.util.HttpUtil;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 
 /**
@@ -15,26 +13,6 @@ import javax.servlet.http.HttpSession;
 public class SessionInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        String token=null;
-        HttpSession session = request.getSession(false);
-        //判断是否已经有登录过的token
-        if(session != null && session.getAttribute("token") != null){
-            token = (String)session.getAttribute("token");
-        }else{
-            token = request.getParameter("token");
-        }
-        System.out.println(token);
-        //验证token的有效性
-        if (token != null) {
-            String reqUrl = "http://www.xiaozhao.com:8090/checkToken";
-            String content = "token=" + token;
-            String result = HttpUtil.sendReq(reqUrl, content);
-            if ("correct".equals(result)) {
-                return true;
-            }
-        }
-        //既没有session会话信息，又没有其他系统已经登录过的token信息的，sso.com进行认证登录
         response.sendRedirect("http://www.xiaozhao.com:8090/preLogin?url=www.xiaozhao.com:8090/xiaozhao/wel");
         return false;
     }
